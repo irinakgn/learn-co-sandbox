f70f1901436f92d0312b53dee32e2227da324d62
Irina Kagan <irinagamb@gmail.com>
2019-11-21 22:16:54 +0000

    Automatically backed up by Learn

---
+++ cli-books/lib/cli/books.rb
@@ -0,0 +1,8 @@
+require "cli/books/version"
+
+module Cli
+  module Books
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ cli-books/lib/cli/books/version.rb
@@ -0,0 +1,5 @@
+module Cli
+  module Books
+    VERSION = "0.1.0"
+  end
+end
